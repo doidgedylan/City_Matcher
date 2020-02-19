2b2a031d8446bdc3618911fe8c6a33ab6d27fd6b
nickmorris991 <nickmorris25@outlook.com>
2020-02-19 15:39:39 -0500

    changed basic question activity layout to use pinners

--- app/src/main/java/com/example/city_matcher/QuestionActivity.java
+++ app/src/main/java/com/example/city_matcher/QuestionActivity.java
@@ -2,12 +2,49 @@ package com.example.city_matcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class QuestionActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuestionActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity);
+        Log.d(TAG, "OnCreate: started.");
+        initSpinners();
+    }
+
+    // ***** HELPER METHODS ***** //
+
+
+    /*
+        method used to initialize and setup the spinners for the personality quiz
+     */
+    private void initSpinners() {
+        Spinner valuesSpinner = findViewById(R.id.valuesSpinner);
+        Spinner industrySpinner = findViewById(R.id.industrySpinner);
+        Spinner distSpinner = findViewById(R.id.distanceSpinner);
+
+        // init adapters
+        ArrayAdapter<String> valQuestionAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.priorityQuestionAnswers));
+        ArrayAdapter<String> indQuestionAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.industryQuestionAnswers));
+        ArrayAdapter<String> distQuestionAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.distanceQuestionAnswers));
+
+        valQuestionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        indQuestionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        distQuestionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        // set adapters
+        valuesSpinner.setAdapter(valQuestionAdapter);
+        industrySpinner.setAdapter(indQuestionAdapter);
+        distSpinner.setAdapter(distQuestionAdapter);
     }
 }
--- app/src/main/java/com/example/city_matcher/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/city_matcher/RecyclerViewAdapter.java
@@ -1,15 +1,71 @@
 package com.example.city_matcher;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import android.view.View;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
     private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> answers = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> answers) {
+        this.answers = answers;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        holder.questionText.setText(answers.get(position));
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "OnClick: clicked on: " + answers.get(position));
+
+                Toast.makeText(mContext, answers.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return answers.size();
+    }
 
     // holds views in memory ready to add the next ones
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView questionText;
+        LinearLayout parentLayout;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            // attach widgets to id's
+            questionText = itemView.findViewById(R.id.questionText);
+            parentLayout = itemView.findViewById(R.id.parentLayout);
+
+
         }
     }
 }
