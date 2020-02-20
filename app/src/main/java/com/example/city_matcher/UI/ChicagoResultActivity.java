1db0cd292d6bbb738c214d85263622a2727700ab
nickmorris991 <nickmorris25@outlook.com>
2020-02-20 15:53:39 -0500

    added fragment injection to result page with fragment specific lifecycle methods.

--- app/src/main/java/com/example/city_matcher/UI/ChicagoResultActivity.java
+++ app/src/main/java/com/example/city_matcher/UI/ChicagoResultActivity.java
@@ -2,8 +2,11 @@ package com.example.city_matcher.UI;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.city_matcher.R;
 
@@ -16,4 +19,6 @@ public class ChicagoResultActivity extends AppCompatActivity {
         setContentView(R.layout.chicago_result_activity);
         Log.d(TAG, "OnCreate: started.");
     }
+
 }
+
