098b5e7a09f4fd961c8bdf1a0fb6b5a7e48bdb1f
nickmorris991 <nickmorris25@outlook.com>
2020-03-04 21:55:50 -0500

    firebase setup and basic update operation finished

--- app/src/main/java/com/example/city_matcher/UI/LoginFragment.java
+++ app/src/main/java/com/example/city_matcher/UI/LoginFragment.java
@@ -12,35 +12,55 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import com.example.city_matcher.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginFragment extends Fragment {
-    EditText usernameEdit;
-    Button loginButton;
-
-    String username;
-    String password;
 
     private static final String TAG = "LoginFragment";
 
+    // define UI elements
+    EditText usernameEdit;
+    EditText passwordEdit;
+    Button loginButton;
+    String username;
+    String password;
+
+    // get Firebase reference
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mAccountsRef = mRootRef.child("accounts");
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.login_fragment, container, false);
 
-
-        // get reference
+        // get UI elements
         usernameEdit = (EditText) v.findViewById(R.id.username);
+        passwordEdit = (EditText) v.findViewById(R.id.password);
         loginButton = (Button) v.findViewById(R.id.submitLogin);
+        return v;
+    }
 
-        // onClickListeners
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // set listeners
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // get username and password
                 username = usernameEdit.getText().toString();
+                password = passwordEdit.getText().toString();
+
+                // write them to real time database
+                //String id = mAccountsRef.push().getKey();
+                
+                mAccountsRef.setValue(username);
+
                 Toast.makeText(getContext(), username, Toast.LENGTH_SHORT).show();
             }
         });
-        return v;
     }
 }
