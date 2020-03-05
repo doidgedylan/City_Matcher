44d4b547711f974ed0cadb24a60420ebac540784
nickmorris991 <nickmorris25@outlook.com>
2020-03-05 03:58:31 -0500

    cleanup commit

--- app/src/main/java/com/example/city_matcher/UI/QuestionActivity.java
+++ app/src/main/java/com/example/city_matcher/UI/QuestionActivity.java
@@ -16,53 +16,30 @@ import com.example.city_matcher.R;
 public class QuestionActivity extends AppCompatActivity {
 
     private static final String TAG = "QuestionActivity";
-    private Button button;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity);
         Log.d(TAG, "OnCreate: hit and activated");
+
+        // initialize spinners
         initSpinners();
 
-        //handle submit button
-        Button submitButton = (Button) findViewById(R.id.submitButton);
+        //grab ui reference
+        submitButton = (Button) findViewById(R.id.submitButton);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openResult();
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "OnStart: on the quiz page.");
-    }
-
-    @Override
-    protected void onResume () {
-        super.onResume();
-        Log.d(TAG, "OnResume: returned to the quiz page.");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "OnPause: paused to go to results activity");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "OnStop: Stopped activity");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "OnDestroy: Stopped activity");
     }
 
     // ***** PRIVATE HELPER METHODS ***** //
@@ -93,13 +70,11 @@ public class QuestionActivity extends AppCompatActivity {
         indQuestionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         drinkQuestionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         distQuestionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-
-
+        
         // set adapters
         valuesSpinner.setAdapter(valQuestionAdapter);
         industrySpinner.setAdapter(indQuestionAdapter);
         drinkSpinner.setAdapter(drinkQuestionAdapter);
         distSpinner.setAdapter(distQuestionAdapter);
-
     }
 }
